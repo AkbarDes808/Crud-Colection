97335d7e00d745906131e67109483de9d41d8d56
akbar <akbar.26@students.amikom.ac.id>
2020-06-30 21:53:46 +0800

    tugas 9

---
+++ Crud Collection/Crud Collection/Karyawan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crud_Collection
+{
+    public abstract class Karyawan
+    {
+        public string NIK;
+        public string Nama;
+        public abstract double Gaji();
+    }
+}
---
+++ Crud Collection/Crud Collection/karywan tetap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crud_Collection
+{
+    public class KaryawanTetap : Karyawan
+    {
+
+        public double GajiBulanan;
+        public override double Gaji()
+        {
+            return this.GajiBulanan;
+        }
+    }
+}
